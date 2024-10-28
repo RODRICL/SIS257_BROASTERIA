d8b8dd3b50a7085b37eaf36d3c8dbe11d6c8cae5
Sebastian Gironda <girondasebastian@gmail.com>
2024-10-28 01:17:33 -0400

    Creacion del crud de Categoria

---
+++ backend_broasteria/src/categorias/entities/categoria.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('categorias')
+export class Categoria {
+  @PrimaryGeneratedColumn('identity')
+  id: number;
+
+  @Column('varchar', { length: 30 })
+  nombre: string;
+
+  @Column('varchar', { length: 250 })
+  descripcion: string;
+}
--- backend_broasteria/src/main.ts
+++ backend_broasteria/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('API Rest Laboratorio SIS257')
+    .setDescription('API Rest de la broasteria')
+    .setVersion('1.0')
+    .addTag('categoria')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('apidoc', app, document);
+
+  await app.listen(process.env.PORT);
+  console.log(`App corriendon ${await app.getUrl()}/apidoc`);
 }
 bootstrap();
