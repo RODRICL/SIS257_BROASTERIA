93aea4200be5a0d50debc2792edd0e5d0a1530ba
Sebastian Gironda <girondasebastian@gmail.com>
2024-11-19 21:50:33 -0400

    implementacion de jwt en frontend

--- frontend_broasteria/src/App.vue
+++ frontend_broasteria/src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { useAuthStore } from '@/stores/index'
+const authStore = useAuthStore()
 </script>
 
 <template>
@@ -8,14 +10,19 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld :msg="'BROASTERIA ' + (authStore.user ? ' - Hola ' + authStore.user : '')" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/productos">Producto</RouterLink>
-        <RouterLink to="/clientes">Cliente</RouterLink>
-        <RouterLink to="/ventas">Nueva Venta</RouterLink>
+        <RouterLink to="/">Inicio</RouterLink>
+        <RouterLink v-if="!authStore.token" to="/login">Iniciar Sesión</RouterLink>
+        <span v-else>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/productos">Producto</RouterLink>
+          <RouterLink to="/clientes">Cliente</RouterLink>
+          <RouterLink to="/ventas">Nueva Venta</RouterLink>
+          <a @click="authStore.logout()" class="text-danger">Salir</a>
+        </span>
       </nav>
     </div>
   </header>
